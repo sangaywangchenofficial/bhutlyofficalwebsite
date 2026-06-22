3c7c9910facb357e74d034c3ac80f3302ec973a9
Sangay Wangchen <sangay.wangchen.official@gmail.com>
2026-06-22 17:28:29 +0600

    Page not found updated

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-6xl font-bold">404 - Page Not Found!</h1>
+                <p className="mt-4 text-gray-600">
+                    Sorry, the page you are looking for does not exist.
+                </p>
+            </div>
+        </div>
+    );
+}
